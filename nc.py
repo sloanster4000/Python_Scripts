ecac6f8e17c2241ddd0066064e183ea9a8311468
sloanster4000 <sloanster4000@gmail.com>
2020-05-28 10:10:28 -0500

    Update nc.py
    Added options for UDP sockets

--- nc.py
+++ nc.py
@@ -5,6 +5,7 @@
 # 06/05/2017:   Uploaded to github.[ERS]
 # 05/21/2020:   Added socket timeout settings, changed port type as int and removed -v switch. [ERS]
 # 05/22/2020:   Added sys exceptions. [ERS]
+# 05/28/2020:   Added flag for UDP connections. [ERS]
 import sys
 import socket
 import argparse
@@ -13,24 +14,47 @@ import argparse
 parser = argparse.ArgumentParser(description='Netcat like port checker')
 parser.add_argument('host', help="Host address")
 parser.add_argument('port', help="Host port", type=int)
+parser.add_argument('-u', '--udp', help="Attempt a UDP connection", action='store_true')
 args = parser.parse_args()
 
-#Attempt a connection
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.settimeout(5.5)
-    s.connect((args.host, args.port))
-    print(args.host, args.port, "open")
-    s.shutdown(socket.SHUT_WR)
-    s.close()
+#Attempt a UDP connection
+if args.udp == False:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(5.5)
+        s.connect((args.host, args.port))
+        print(args.host, args.port, "TCP port open")
+        s.shutdown(socket.SHUT_WR)
+        s.close()
 
-except KeyboardInterrupt:
-    sys.exit()
+    except KeyboardInterrupt:
+        sys.exit()
 
-except socket.gaierror:
-    print ('Hostname could not be resolved')
-    sys.exit()
+    except socket.gaierror:
+        print ('Hostname could not be resolved')
+        sys.exit()
 
-except socket.error:
-    print ('Could not connect to server')
-    sys.exit()
+    except socket.error:
+        print ('Could not connect to server')
+        sys.exit()
+
+#Attempt a TCP connection
+if args.udp == True:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.settimeout(5.5)
+        s.connect((args.host, args.port))
+        print(args.host, args.port, "UDP port open")
+        s.shutdown(socket.SHUT_WR)
+        s.close()
+
+    except KeyboardInterrupt:
+        sys.exit()
+
+    except socket.gaierror:
+        print ('Hostname could not be resolved')
+        sys.exit()
+
+    except socket.error:
+        print ('Could not connect to server')
+        sys.exit()
