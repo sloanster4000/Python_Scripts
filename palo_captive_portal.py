970cd30998f21e35f20a2c799582a7cbcc8d2b22
sloanster4000 <17884547+sloanster4000@users.noreply.github.com>
2023-09-14 10:42:53 -0400

    Update palo_captive_portal.py
    pep8 formatting and functions

--- palo_captive_portal.py
+++ palo_captive_portal.py
@@ -1,19 +1,41 @@
 #!/usr/bin/env python3
-import requests
+"""Script authenticate with Palo Alto FW Captive Portal"""
 from getpass import getpass
+import logging
+import requests
+import urllib3
 from bs4 import BeautifulSoup
-from requests.packages import urllib3
-requests.packages.urllib3.disable_warnings()
+urllib3.disable_warnings()
 
-user = input('username: ')
-pw = getpass("Enter password for " + user + " : ")
-try:
-    url = "https://IPADDRESS:PORT/php/uid.php?vsys=1&rule=0"
-    payload = ("escapeUser={}&user={}&passwd={}&ok=Login").format(user, user, pw)
-    r = requests.post(url, verify=False, data=payload)
-    soap = BeautifulSoup(r.text, 'html.parser')
-    print(soap.b.string)
-except requests.exceptions.HTTPError as err:
-    print('Http Error Code ' + str(err))
-except requests.exceptions.RequestException as e:
-    print('A connection error occurred: ' + str(e))
+def start_logger():
+    """Function for Logging"""
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)-15s - [%(levelname)-5s] - [%(module)-15.15s] - line_number [%(lineno)3s] - %(message)s",
+        handlers=[
+            logging.StreamHandler()
+        ]
+    )
+
+
+def captive_portal(user, pswd, site):
+    """Function to authenticate with Palo Alto FW Captive Portal"""
+    for site in site.split(' '):
+        url = f"https://{site}:6082/php/uid.php?vsys=1&rule=0"
+        payload = f"escapeUser={user}&user={user}&passwd={pswd}&ok=Login"
+        req = requests.post(url, verify=False, data=payload, timeout=10)
+        soap = BeautifulSoup(req.text, 'html.parser')
+        logging.info('Authenticated with %s', site)
+        print(soap.b.string)
+
+
+if __name__ == '__main__':
+    USER = input('username: ')
+    PWD = getpass("Enter password for " + USER + " : ")
+    SITE = input('Site Code: ')
+    try:
+        captive_portal(USER, PWD, SITE)
+    except requests.exceptions.HTTPError as err:
+        logging.error('Http Error Code %s', err)
+    except requests.exceptions.RequestException as e:
+        logging.error('A connection error occurred: %s', e)
